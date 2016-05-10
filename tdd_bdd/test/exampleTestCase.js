91b44503af9db8960d3cdebf18533f9893ea3f5c
gaupoit <gaupoit@outlook.com>
2016-05-10 14:51:50 +0800

    fix

--- tdd_bdd/test/exampleTestCase.js
+++ tdd_bdd/test/exampleTestCase.js
@@ -4,13 +4,12 @@ const expect = require('chai').expect;
 const client = require('./client');
 
 describe('Test example.com', function(){
-    before(function(done) {
-        client.init().url('http://example.com', done);
+    before(function() {
     });
 
     describe('Check homepage', function(){
         it('should see the correct title', function(done) {
-            client.getTitle().then(function(title){
+            client.init().url('http://example.com').getTitle().then(function(title){
             	console.log(title);
                 expect(title).to.have.string('Example Domain');
                 done();
